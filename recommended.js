8ab4fed9c0d99ad6a5c13af11fb69d7dee09fa62
Eliott Vincent <dev@eliottvincent.com>
2023-07-19 17:16:02 +0200

    Cleanup

--- recommended.js
+++ recommended.js
@@ -1,26 +1,31 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true
+  env: {
+    es6: true,
+    node: true
   },
-  "settings": {
-    "jsdoc": {
-      "tagNamePreference": {
-        "returns": "return"
+
+  settings: {
+    jsdoc: {
+      tagNamePreference: {
+        returns: "return"
       },
 
-      "preferredTypes": {
-        "Function": "function"
+      preferredTypes: {
+        Function: "function"
       }
     }
   },
-  "plugins": [
+
+  plugins: [
     "eslint-plugin-jsdoc",
   ],
-  "extends": [
+
+  extends: [
     "plugin:jsdoc/recommended"
   ],
-  "rules": {
+
+  rules: {
+    // General JS rules
     "no-eval": "error",
     "no-console": "warn",
     "no-debugger": "warn",
@@ -32,6 +37,13 @@ module.exports = {
     "max-len": ["error", 80],
     "comma-dangle": ["error", "never"],
     "arrow-parens": ["error", "always"],
+
+    // Crisp JSDoc rules
+    "crisp/jsdoc-enforce-classdesc": "error",
+
+    // JSDoc rules
+    "jsdoc/require-param-description": "off",
+    "jsdoc/newline-after-description": "off",
     "jsdoc/require-jsdoc": [
       "error",
 
@@ -45,8 +57,8 @@ module.exports = {
         }
       }
     ],
-    "jsdoc/require-param-description": "off",
-    "jsdoc/newline-after-description": "off",
+
+    // Crisp rules
     "crisp/align-one-var": ["error"],
     "crisp/multiline-comment-end-backslash": "error",
     "crisp/const": "error",
@@ -62,7 +74,6 @@ module.exports = {
     "crisp/variable-names": ["error", {
       "variableExceptions": ["fn"]
     }],
-
     "crisp/constructor-variables": ["error", {
       "filenameExceptions": ["app.js"],
       "variableExceptions": ["client"]
