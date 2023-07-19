dc19214676eb543aa21d9e4b5d4856546301681a
Eliott Vincent <dev@eliottvincent.com>
2023-07-19 17:23:36 +0200

    Typo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = {
     "constructor-variables": require("./rules/constructor-variables"),
     "jsdoc-enforce-classdesc": require("./rules/jsdoc-enforce-classdesc"),
     "methods-naming": require("./rules/methods-naming"),
-    "multiline-comment-end-backslash": require("./rules/multiline-comment-end-backslash")
+    "multiline-comment-end-backslash": require("./rules/multiline-comment-end-backslash"),
     "new-line-after-block": require("./rules/new-line-after-block"),
     "no-async": require("./rules/no-async"),
     "no-trailing-spaces": require("./rules/no-trailing-spaces"),
@@ -17,6 +17,6 @@ module.exports = {
     "regex-in-constructor": require("./rules/regex-in-constructor"),
     "ternary-parenthesis": require("./rules/ternary-parenthesis"),
     "two-lines-between-class-members": require("./rules/two-lines-between-class-members"),
-    "variable-names": require("./rules/variable-names"),
+    "variable-names": require("./rules/variable-names")
   }
 };
