5d36591be6918f84d9270827bf2f7e3ca045b392
Eliott Vincent <dev@eliottvincent.com>
2023-07-20 14:02:35 +0200

    Support Line comments

--- rules/multiline-comment-end-backslash.js
+++ rules/multiline-comment-end-backslash.js
@@ -14,7 +14,10 @@ module.exports = {
         const sourceCode = context.getSourceCode();
         const comments = sourceCode.getAllComments();
 
-        comments.forEach(comment => {
+        let lineCommentGroup = [];
+
+        comments.forEach((comment, i) => {
+          // Handle Block comments
           if (
             comment.type === "Block" &&
             comment.loc.start.line !== comment.loc.end.line &&
@@ -26,8 +29,31 @@ module.exports = {
               message: "Multiline comments should end with a backslash '\\', unless they are JSDoc comments.",
             });
           }
+
+          // Handle Line comments
+          if (comment.type === "Line") {
+            lineCommentGroup.push(comment);
+            const nextComment = comments[i + 1];
+
+            if (nextComment && nextComment.loc.start.line - 1 === comment.loc.end.line) {
+              // If the current comment is not the last in the group and does not end with a backslash,
+              // report an error
+              if (!comment.value.trim().endsWith('\\')) {
+                context.report({
+                  node: comment,
+                  message: "This line of a multiline comment should end with a backslash '\\'.",
+                });
+              }
+
+              // This is not the last comment in the group, continue to next iteration
+              return;
+            }
+          }
+
+          // Reset line comment group
+          lineCommentGroup = [];
         });
-      },
-    };
-  },
+      }
+    }
+  }
 };
