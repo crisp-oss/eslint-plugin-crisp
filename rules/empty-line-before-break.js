861328ffd77969bb1ea034695484508228d73e3c
Eliott Vincent <dev@eliottvincent.com>
2023-07-25 10:54:20 +0200

    Support empty case and if statements

--- rules/empty-line-before-break.js
+++ rules/empty-line-before-break.js
@@ -12,27 +12,34 @@ module.exports = {
   create(context) {
     return {
       "BreakStatement": function (node) {
+        // Get source code of the file.
         const sourceCode = context.getSourceCode();
 
         // Get the token before the "break" keyword. This could be either a \
-        //   statement, or the opening brace of a case clause.
+        //   statement, or the opening brace of an "if" or "case" clause.
         const tokenBefore = sourceCode.getTokenBefore(node, { includeComments: true });
 
         // Get the token two places before the "break" keyword.
         const tokenTwoBefore = sourceCode.getTokenBefore(tokenBefore, { includeComments: true });
 
-        // Retrieve the switch case statement related to the current break \
+        // Retrieve the parent BlockStatement related to the current break \
         //   statement.
-        const caseToken = sourceCode.getNodeByRangeIndex(tokenBefore.range[0] - 2);
+        const parentBlockStatement = node.parent;
 
-        // If the caseToken exists and has one or no statements (meaning it \
-        //   only contains a break statement), we do not enforce a new line \
-        //   before the "break" statement.
-        if (caseToken && caseToken.test && caseToken.consequent.length <= 1){
+        // Retrieve the parent Statement of the BlockStatement
+        const parentStatement = parentBlockStatement.parent;
+
+        // If the parentStatement is an IfStatement or SwitchCase and the \
+        //   parentBlockStatement contains only the "break" statement, we \
+        //   do not need to enforce a new line before the "break" statement.
+        if(parentBlockStatement.body.length === 1 &&
+           ((parentStatement.type === "IfStatement") ||
+            (parentStatement.type === "SwitchCase"))){
           return;
         }
 
-        // If there's not an empty line before the "break" statement (excluding comments), report an error.
+        // If there's not an empty line before the "break" statement \
+        //   (excluding comments), report an error.
         if (tokenTwoBefore && tokenTwoBefore.loc.end.line === node.loc.start.line - 1) {
           context.report({
             node,
