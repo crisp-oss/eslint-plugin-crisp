bfa1b6939bbfaf823bd5f2089911dbb31db91f6b
Eliott Vincent <dev@eliottvincent.com>
2023-08-29 09:00:03 +0200

    v1.0.49
    Add other pattern for enforce-optional

--- rules/enforce-optional.js
+++ rules/enforce-optional.js
@@ -11,30 +11,54 @@ module.exports = {
 
   create(context) {
     const sourceCode = context.getSourceCode();
-    // Search for `foo && foo.` or `foo && foo[`
-    // Notice: do not match `foo && foo.bar !==` or `foo && foo[bar] !==`, \
-    //   as the optional version would be unsafe
-    const pattern = /\b(\w\S+)\s&&\s+\1\b[\[\.](?!\w\S+\s\!\=\=)/g;
+
+    const patterns = [
+      // Search for `foo && foo.` or `foo && foo[`
+      // Notice: do not match `foo && foo.bar !==` or `foo && foo[bar] !==`, \
+      //   as the version with optional chaining would be unsafe
+      /\b(\w\S+)\s&&\s+\1\b[\[\.](?!\w\S+\s\!\=\=)/g,
+
+      // Search for patterns like `foo || {}).bar`, `foo || {})[`, \
+      //   `foo || []).bar` or `foo || [])[`
+      // Notice: do not match if the value after the dot is "filter", "find", \
+      //   "findIndex", "map", "reduce", "length <", "length === 0", or \
+      //   "length == 0", as the version with optional chaining would be unsafe
+      /\((.+?)\s\|\|\s(\{\}|\[\])\)(\[|\.(?!some\b|filter\b|find\b|findIndex\b|reduce\b|map\b|length\s*<|length\s*===\s*0|length\s*==\s*0))\w*/g
+    ];
 
     return {
       Program() {
         const text = sourceCode.getText();
 
-        let match;
+        patterns.forEach((pattern, index) => {
+          let match;
 
-        while ((match = pattern.exec(text)) !== null) {
-          const start = match.index;
-          const end = start + match[0].length;
-          const range = [start, end];
+          // Reset lastIndex in case the RegExp object is reused
+          pattern.lastIndex = 0;
 
-          context.report({
-            message: "Prefer using optional chaining rather than: '{{codeFragment}}'.",
-            data: {
-              codeFragment: match[0]
-            },
-            loc: sourceCode.getLocFromIndex(range[0])
-          });
-        }
+          while ((match = pattern.exec(text)) !== null) {
+            const start = match.index;
+            const end = start + match[0].length;
+
+            const range = [start, end];
+
+            if (
+              index === 1 &&
+                /Object\.(keys|values|entries)\(/.test(match[1])
+            ) {
+              // Ignore `Object.keys(foo || {})` (and others) as it cannot be \
+              //   safely transformed to optional chaining
+            } else {
+              context.report({
+                message: "Prefer using optional chaining rather than: '{{codeFragment}}'.",
+                data: {
+                  codeFragment: match[0]
+                },
+                loc: sourceCode.getLocFromIndex(range[0])
+              });
+            }
+          }
+        });
       }
     };
   }
