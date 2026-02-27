d30853fc053b966a7071a866e0811f7c24017b21
Baptiste Jamin <baptiste@crisp.chat>
2026-02-27 13:23:04 +0100

    Fix codestyle

--- recommended.js
+++ recommended.js
@@ -92,7 +92,10 @@ export default function configRecommended(pluginCrisp) {
         ],
         "keyword-spacing": "error",
         "linebreak-style": ["error", "unix"],
-        "max-len": ["error", { "code": 80, "ignorePattern": "^/\\*\\* @import" }],
+        "max-len": ["error", {
+          "code": 80,
+          "ignorePattern": "^\\*\\* \\@import"
+        }],
         "no-console": "warn",
         "no-debugger": "warn",
         "no-eval": "error",
